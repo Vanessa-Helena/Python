a9894b1ceedfc17c23d1d127f305deae19760be0
Vanessa-Helena <58368539+Vanessa-Helena@users.noreply.github.com>
2020-06-22 10:18:37 -0300

    Atualização Lançador de Notas

--- PROJETO_FINAL.py
+++ PROJETO_FINAL.py
@@ -31,6 +31,42 @@ while (novo == 'S'):
 
     nota1 = int(input('\nInforme a primeira nota: '))
     while (nota1 <0 or nota1 >10):
+
+titulo = '\nPROJETO FINAL DO CURSO DE INTRODUÇÃO Á PROGRAMAÇÃO PYTHON\n'
+print(titulo.center(20,'*'))
+
+novo = input('Deseja iniciar o cadastro das notas do aluno (S/N)? ').upper()
+while (novo != 'S' and novo != 'N'):
+    print('Opção Invalida')
+    novo = input('Informe "S" para Sim e "N" para Não: ').upper()
+
+totalAlunos = 0
+totalAprovados_F = 0
+totalAprovados_M = 0
+totalExame_F = 0
+totalExame_M = 0
+totalReprovados_F = 0
+totalReprovados_M = 0
+totalAlunos_Feminino = 0
+totalAlunos_Masculino = 0
+
+while (novo == 'S'):
+    totalAlunos += 1
+    nome_aluno = input('\nInforme o nome do aluno: ')
+
+    sexo_aluno = input('Informe o sexo do aluno (F/M): ').upper()
+    while (sexo_aluno != 'F' and sexo_aluno != 'M'):
+        print('Opção Invalida')
+        sexo_aluno = input('Informe "F" para o sexo Feminino e "M" para o sexo Masculino: ').upper()
+
+    #alunos por sexo
+    if (sexo_aluno == 'F'):
+        totalAlunos_Feminino += 1
+    else:
+        totalAlunos_Masculino += 1
+
+    nota1 = int(input('\nInforme a primeira nota: '))
+    while (nota1 <0 or nota1 >10):
         print('Opção Invalida')
         nota1 = input('O valor da nota deve estar entre "0" e "10", digite novamente: ')
 
@@ -40,6 +76,7 @@ while (novo == 'S'):
         nota2 = input('O valor da nota deve estar entre "0" e "10", digite novamente: ')
 
     nota3 = int(input('Informe a terceira nota: '))
+    while (nota3 < 0 or nota3 > 10):
         print('Opção Invalida')
         nota3 = input('O valor da nota deve estar entre "0" e "10", digite novamente: ')
 
@@ -47,14 +84,26 @@ while (novo == 'S'):
 
     #teste de notas
 
-    if (media >= 7):
-        totalAprovados +=1
-    elif (media >= 4 and 7):
-        totalExame +=1
-    else: (media <4)
-    totalReprovados +=1
+    if (sexo_aluno == 'F'):
+            media >= 7
+            totalAprovados_F +=1
+    else:
+            media >= 7
+            totalAprovados_M +=1
 
+    if(sexo_aluno == 'F'):
+            media >= 4 and 7
+            totalExame_F +=1
+    else:
+            media >= 4 and 7
+            totalExame_M +=1
 
+    if(sexo_aluno == 'F'):
+            media <4
+            totalReprovados_F +=1
+    else:
+            media < 4
+            totalReprovados_M +=1
 
     novo = input('\nDeseja continuar o cadastro das notas do aluno (S/N)? ').upper()
     while (novo != 'S' and novo != 'N'):
@@ -64,17 +113,17 @@ else:
     resultados = '\n\nApesentar os resultados finais\n\n'
     print(resultados.center(20,"*"))
 
-print('Percentual de alunos Aprovados: ', (totalAprovados*100)/totalAlunos, '%')
-print('Percentual de alunos de Exame: ', (totalExame*100)/totalAlunos, '%')
-print('Percentual de alunos Reprovados: ', (totalReprovados*100)/totalAlunos, '%')
+print('Total de Alunos Cadastrados', totalAlunos)
+
+print('Percentual de alunos Aprovados: ', (totalAprovados_F + totalAprovados_M*100)/totalAlunos, '%')
+print('Percentual de alunos de Exame: ', (totalExame_F + totalExame_M*100)/totalAlunos, '%')
+print('Percentual de alunos Reprovados: ', (totalReprovados_F + totalReprovados_M*100)/totalAlunos, '%')
 
 print('Total de alunos do sexo Feminino: ', totalAlunos_Feminino)
 print('Total de alunos do sexo Masculino: ', totalAlunos_Masculino)
-print('Total de alunos do sexo Feminino Aprovados: ' )
-print('Total de alunos do sexo Masculino Aprovados: ')
-print('Total de alunos do sexo Feminino de Exame: ')
-print('Total de alunos do sexo Masculino de Exame: ')
-print('Total de alunos do sexo Feminino Reprovados: ')
-print('Total de alunos do sexo Masculino Reprovados: ')
-
-
+print('Total de alunos do sexo Feminino Aprovados: ', totalAprovados_F )
+print('Total de alunos do sexo Masculino Aprovados: ', totalAprovados_M)
+print('Total de alunos do sexo Feminino de Exame: ', totalExame_F)
+print('Total de alunos do sexo Masculino de Exame: ', totalExame_M)
+print('Total de alunos do sexo Feminino Reprovados: ', totalReprovados_F)
+print('Total de alunos do sexo Masculino Reprovados: ', totalReprovados_M)
